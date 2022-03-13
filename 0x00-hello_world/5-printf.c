edd7e853114d5b9634b676e311f16cb08619b658
Ifeanyik <jax64720@gmail.com>
2022-03-13 16:49:29 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Description
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("without proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
