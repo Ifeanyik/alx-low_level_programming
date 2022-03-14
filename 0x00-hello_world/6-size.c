260a805ea2e5ab00268678f4c08a5bbee071953d
Ifeanyik <jax64720@gmail.com>
2022-03-13 17:09:49 -0700

    Error fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,12 +9,12 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float e;
-	
-	printf("Size of a char: %zu byte(s)/n",sizeof(a));
-	printf("Size of an int: %zu byte(s)/n", sizeof(b));
-	printf("Size of long int: %zu byte(s)/n", sizeof(c));
-	printf("Size of a long long int: %zu bytes(s)/n", sizeof(d));
-	printf("Size of a float: %zu byte(s)/n", sizeof(e));
+	float e:
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+	printf("Size of an int: %zu byte(s)\n", sizeof(b));
+	printf("Size of long int: %zu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %zu bytes(s)\n", sizeof(d));
+	printf("Size of a float: %zu byte(s)\n", sizeof(e));
 	return (0);
 }
