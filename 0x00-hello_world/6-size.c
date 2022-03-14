710bc9c85159592d356cdc6c99af3193589fd020
Ifeanyik <jax64720@gmail.com>
2022-03-13 17:11:46 -0700

    Error fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float e:
+	float e;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(a));
 	printf("Size of an int: %zu byte(s)\n", sizeof(b));
