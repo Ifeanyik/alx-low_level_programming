9ca66462a376c8a6a4844c3ed9037e76de8c46f9
Ifeanyik <jax64720@gmail.com>
2022-03-15 12:58:40 -0700

    [fix] Print signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int n)
 {
 	if (n > 1)
 	{
-		write(1, "+", 2);
+		write(1, "+", 1);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		write(1, "0", 2);
+		write(1, "0", 1);
 		return (0);
 	}
 	else
 	{
-		write(1, "-", 2);
-		return(-1);
+		write(1, "-", 1);
+		return (-1);
 	}
 }
