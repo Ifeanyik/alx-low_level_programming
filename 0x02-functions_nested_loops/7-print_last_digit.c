925658d61e75919cce68f6923a7e5c0885277ca3
Ifeanyik <jax64720@gmail.com>
2022-03-15 13:32:03 -0700

    Get last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * print_last_digit - prints last digit
+ * @a: parameter to be passed
+ * Return: last digit of value
+ */
+
+int print_last_digit(int a)
+{
+	int m = a % 10;
+	write(1, m, 1);
+	return (m);
+}
