09bf0db28a27ef44546b4f3bd48596bb97798df8
Ifeanyik <jax64720@gmail.com>
2022-03-15 16:47:34 -0700

    h

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,10 +5,17 @@
  * @a: parameter to be passed
  * Return: last digit of value
  */
+int putchar(char c;)
 
 int print_last_digit(int a)
 {	
 	int m = a % 10;
+	write(1, &m, 1);
 	putchar(m + '0');
 	return (m);
+}
+
+int putchar(char c)
+{
+	return(write(1, &c, 1));
 }
