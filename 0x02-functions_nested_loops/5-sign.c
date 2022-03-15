dd902a71e9963c6ed2ff92202d16b0d8a116a834
Ifeanyik <jax64720@gmail.com>
2022-03-15 12:54:59 -0700

    Print signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+/**
+ * print_sign - Returns 1 if number is greater than 0
+ * 0 if number is equat to 0
+ * -1 if number is less than 0
+ *  @n: parameter to be passed
+ * Return: 1 if num positive
+ * 0 if num is zero
+ * -1 if num is negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 1)
+	{
+		write(1, "+", 2);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		write(1, "0", 2);
+		return (0);
+	}
+	else
+	{
+		write(1, "-", 2);
+		return(-1);
+	}
+}
