651f6fc8b23db68fea009a41c3f6b2020d9f11be
Ifeanyik <jax64720@gmail.com>
2022-03-15 11:29:06 -0700

    Main

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
  * main - Betty's annoying
- *print_alphabet - Prints all alphabets in lower case
  * Return: Always 0
  */
 
 void print_alphabet(void)
 {
+	//prints all alphabets in lower case
 	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
 }
