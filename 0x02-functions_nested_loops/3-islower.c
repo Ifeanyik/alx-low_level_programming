6695bbbceed05cc24b926334885876077a0751bb
Ifeanyik <jax64720@gmail.com>
2022-03-15 12:37:11 -0700

    [fix] Parameter description

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,8 @@
 
 /**
  * _islower - checks if character is lower case
- *c - Parameter to be accepted in _islower
- *Return: Always 0
+ *@c - Parameter to be accepted in _islower
+ *Return: 1 if character is in lower case
  */
 
 int _islower(int c)
