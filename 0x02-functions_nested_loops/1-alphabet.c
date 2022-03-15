9fc8247dd7658d133c8e04cf583b174a8b28d808
Ifeanyik <jax64720@gmail.com>
2022-03-15 09:46:36 -0700

    Final flash

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,10 @@
 #include <unistd.h>
+/**
+ * main - Betty's annoying
+ *Return: Always 0
+ /
 
 void print_alphabet(void)
 {
-	write(1, "abcdefghijklmnopqrstuvwxyz", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
 }
