444e9c1c84c3fba8b9d36d404f3f6906bf638c92
Ifeanyik <jax64720@gmail.com>
2022-03-21 10:02:37 -0700

    swapping numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - function name
+ * @a: parameter pointer type
+ * @b: parameter pointer type
+ * Description: swaps values of a and b
+ */
+void swap_int(int *a, int *b)
+{
+	int *c = &a, *d = &b;
+
+	*c = 42;
+	*d = 98;
+}
