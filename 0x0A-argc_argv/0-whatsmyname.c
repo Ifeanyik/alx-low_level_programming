f569df2770a83e5aa457f35a84955746e86bc53d
Ifeanyik <jax64720@gmail.com>
2022-04-01 08:30:10 -0700

    Prints file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints path of program being executed
+ * @argc: number of arguments passed to program
+ * @argv: arrays of arguments passed
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
