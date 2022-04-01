b5f0feeb1afeb232e3e98476b8545d4f22787539
Ifeanyik <jax64720@gmail.com>
2022-04-01 08:53:52 -0700

    Print arg num

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of passed arguments
+ * @argc: number of passed arguments
+ * @argv: array of passed arguments
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__ ((unused)) *argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
