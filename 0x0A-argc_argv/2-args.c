9667e4c768eeaeecac29fbb205e9e24eeb21da4e
Ifeanyik <jax64720@gmail.com>
2022-04-01 09:09:33 -0700

    Prints all args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 0)
+	{
+		int i = 0;
+		for (; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+	}
+}
