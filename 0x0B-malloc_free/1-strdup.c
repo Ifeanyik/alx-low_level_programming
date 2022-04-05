4e0f075bf1bc4868125fabe3b687caf9bc028b8d
Ifeanyik <jax64720@gmail.com>
2022-04-05 03:40:59 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - creates array containing string duplicate
+ * @str: string to be duplicated
+ * Return: array else NULL on failure
+ */
+
+char *_strdup(char *str)
+{
+	char *arr;
+	int str_size = size(str);
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	arr = malloc(sizeof(char) * str_size);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < str_size; i++)
+	{
+		arr[i] = *(str + i);
+		if (*(str + (i + 1)) == '\0')
+			arr[i + 1] = '\0';
+	}
+	return (arr);
+}
+
+/**
+ * size - gets size of given string
+ * @str: string to be counted
+ * Return: size of string
+ */
+
+int size(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
