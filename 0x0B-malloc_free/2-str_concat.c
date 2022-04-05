6d44a8f92a020c3f3165b1b36ab567614dacd594
Ifeanyik <jax64720@gmail.com>
2022-04-05 10:56:28 -0700

    I

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,73 +1,42 @@
-#include "main.h"
-#include <stdlib.h>
+#include "main.h"#include <stdlib.h>
 
 /**
- *size - gets size of given string
- * @str: string parameter
- * Return: size of given string
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: pointer of an array of chars
  */
-
-int size(char *str)
-{
-	int i;
-
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-/**
- * str_concat - concatenates given strings
- * @s1: first given string
- * @s2: second given string
- * Return: NULL if failure else pointer to new string
- */
-
 char *str_concat(char *s1, char *s2)
 {
-	int size_s1, size_s2, sum;
-	char *arr;
-	int i = 0, j = 0;
+	char *strout;
+	unsigned int i, j, k, limit;
 
 	if (s1 == NULL)
-		size_s1 = 0;
-	else
-		size_s1 = size(s1);
+		s1 = "";
 	if (s2 == NULL)
-		size_s2 = 0;
-	else
-		size_s2 = size(s2);
-	if (s1 == NULL && s2 != NULL)
-		sum = size_s2;
-	else if (s1 != NULL && s2 == NULL)
-		sum = size_s1;
-	else if (s1 && s2 != NULL)
-		sum = size_s1 + size_s2;
-	else
-		return (NULL);
+		s2 = "";
 
-	arr = malloc(sum * sizeof(char));
+	for (i = 0; s1[i] != '\0'; i++)
+		;
 
-	if (arr == NULL)
-		return (NULL);
-	if (s1 != NULL)
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	strout = malloc(sizeof(char) * (i + j + 1));
+
+	if (strout == NULL)
 	{
-		while (s1[i] != '\0')
-		{
-			arr[i] = s1[i];
-			i++;
-		}
+		free(strout);
+		return (NULL);
 	}
-	if (s2 != NULL)
-	{		
-		while (s2[j] != '\0')
-		{
-			arr[i] = s2[j];
-			i++;
-			j++;
-			if (s2[j] == '\0')
-				arr[i] = '\0';
-		}
-	}
-	return (arr);
+
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
+
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
+
+	return (strout);
 }
