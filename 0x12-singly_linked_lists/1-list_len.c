21bcabae4e900c6a25c7fb62c90dfa33563f718f
Ifeanyik <jax64720@gmail.com>
2022-04-21 14:16:07 -0700

    Get list lenght

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * list_len - finds length of linked list
+ * @h: pointer to singly linked list
+ * Return: number of elements in list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t ele_num = 0;
+
+	while (h)
+	{
+		if (h->len)
+			ele_num++;
+		h = h->next;
+	}
+	return (ele_num);
+}
