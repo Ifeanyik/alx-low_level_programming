4df5b156970ae7aac41698ba2d232890b58ce5a8
Ifeanyik <jax64720@gmail.com>
2022-04-26 01:25:16 -0700

    Free list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees given linked list
+ * @head: pointer to first list element
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *addr_keep;
+	listint_t *h = *head;
+
+	while (h)
+	{
+		addr_keep = h->next;
+		free(h);
+		h = addr_keep;
+	}
+	*head = NULL;
+}
