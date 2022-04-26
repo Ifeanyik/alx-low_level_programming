56245b203601bebfc43c852e66013e91ca3ec901
Ifeanyik <jax64720@gmail.com>
2022-04-26 06:58:37 -0700

    Get node index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - gets node at given index
+ * @head: pointer to linked list
+ * @index: index of required node
+ * Return: pointer to required node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	do {
+		if (head)
+		{
+			head = head->next;
+			count++;
+		}
+		else
+			break;
+	} while (head && count < index);
+	if (head != NULL)
+		return (head);
+	else
+		return (NULL);
+}
