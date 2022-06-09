b6a2c5ee96228e81fc65dd30be5d1ae032d4bed6
Ifeanyik <jax64720@gmail.com>
2022-06-09 08:04:57 -0700

    Add node at index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,6 +13,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int length;
 	dlistint_t *h;
 
+	if (!head)
+		return (head);
 	h = head;
 	for (length = 0; length < index; length++)
 	{
