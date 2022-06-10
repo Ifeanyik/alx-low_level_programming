f375e0681b231714af3f40e19781d130f2c4d5a3
Ifeanyik <jax64720@gmail.com>
2022-06-10 15:51:06 -0700

    Edge cases fix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,41 +13,48 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *h, *prev_el, *next_el, *head_addr;
-	unsigned int i, length;
+	dlistint_t *h, *prev_el, *next_el, *ind_addr;
+	unsigned int length;
 
 	h = *head;
-	head_addr = *head;
-	for (length = 0; h; length++)
-	       h = h->next;
-	if ((index >= length) || (!index))
+	if (!h)
 		return (-1);
-	h = head_addr;
-	for (i = 0; h; i++)
+	for (length = 0; h; length++)
 	{
-		if (i == index)
-		{
-			/*must add else*/
-			if (i == 0)
-			{
-				*head = h->next;
-				free(h);
-				return (1);
-			}
-			else if (i == (length - 1))
-			{
-				prev_el = h->prev;
-				prev_el->next = NULL;
-				free(h);
-				return (1);
-			}
-			prev_el = h->prev;
-			next_el = h->next;
-			prev_el->next = next_el;
-			next_el->prev = prev_el;
-			free(h);
-		}
-		h = h->next;
+		if (length == index)
+			ind_addr = h;
+	       h = h->next;
 	}
-	return (-1);
+	if ((index >= length) || ((index != 0) && (!index)))
+		return (-1);
+	if (index == 0)
+	{
+		if (ind_addr->next == NULL)
+		{
+			free(ind_addr);
+			*head = NULL;
+			return (1);
+		}
+		else
+		{
+			next_el = ind_addr->next;
+			next_el->prev = NULL;
+			*head = next_el;
+			free(ind_addr);
+			return (1);
+		}
+	}
+	else if (index == (length - 1))
+	{
+		prev_el = ind_addr->prev;
+		prev_el->next = NULL;
+		free(ind_addr);
+		return (1);
+	}
+	prev_el = ind_addr->prev;
+	next_el = ind_addr->next;
+	prev_el->next = next_el;
+	next_el->prev = prev_el;
+	free(ind_addr);
+	return (1);
 }
